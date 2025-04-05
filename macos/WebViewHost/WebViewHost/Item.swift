4426c93e9eb0e88306b30fa04ca059ffd65df0ed
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-04-05 18:21:34 +0200

    build: initialize macos app at macos/WebViewHost

---
+++ macos/WebViewHost/WebViewHost/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WebViewHost
+//
+//  Created by Patryk Mleczek on 4/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ macos/WebViewHost/WebViewHost/WebViewHostApp.swift
@@ -0,0 +1,32 @@
+//
+//  WebViewHostApp.swift
+//  WebViewHost
+//
+//  Created by Patryk Mleczek on 4/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WebViewHostApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
