dd87cefc1425713c001ebe09cf4efae24723b7ae
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-04-12 21:31:25 +0200

    feat: reposition traffic lights on window resize

--- macos/WebViewHost/WebViewHost/Delegates/WindowDelegate.swift
+++ macos/WebViewHost/WebViewHost/Delegates/WindowDelegate.swift
@@ -9,6 +9,12 @@ import AppKit
 
 final class WindowDelegate: NSObject, NSWindowDelegate {
   func windowDidResize(_ notification: Notification) {
-    print("windowDidResize_")
+    if let window = NSApp.windows.first {
+      positionTrafficLights(
+        window,
+        x: WindowConstants.TRAFFIC_LIGHTS_INSET_X,
+        y: WindowConstants.TRAFFIC_LIGHTS_INSET_Y
+      )
+    }
   }
 }
