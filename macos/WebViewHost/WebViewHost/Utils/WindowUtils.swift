49a967e451a0c462618d37b8cc7eda6d2559777a
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-04-12 20:59:10 +0200

    feat: add function for custom traffic lights positioning

--- macos/WebViewHost/WebViewHost/Utils/WindowUtils.swift
+++ macos/WebViewHost/WebViewHost/Utils/WindowUtils.swift
@@ -12,3 +12,38 @@ func hideTitleBar(_ window: NSWindow) {
   window.titlebarAppearsTransparent = true
   window.backgroundColor = NSColor(red: 0.071, green: 0.071, blue: 0.071, alpha: 1.0)
 }
+
+func positionTrafficLights(_ window: NSWindow, x: CGFloat, y: CGFloat) {
+  guard let closeButton = window.standardWindowButton(.closeButton),
+        let minimizeButton = window.standardWindowButton(.miniaturizeButton),
+        let fullscreenButton = window.standardWindowButton(.zoomButton)
+  else {
+    return
+  }
+  
+  let titleBar = closeButton.superview?.superview
+  let windowButtonHeight = closeButton.frame.height
+  
+  if let titleBar {
+    let titleBarHeight = windowButtonHeight + y
+    var titleBarFrame = titleBar.frame
+    titleBarFrame.size.height = titleBarHeight
+    titleBarFrame.origin.y = window.frame.size.height - titleBarHeight
+    titleBar.frame = titleBarFrame
+    
+    var closeButtonFrame = closeButton.frame
+    var minimizeButtonFrame = minimizeButton.frame
+    var fullscreenButtonFrame = fullscreenButton.frame
+    
+    let buttonSpacingX = minimizeButtonFrame.origin.x - closeButtonFrame.origin.x
+    
+    closeButtonFrame.origin.x = x
+    closeButton.setFrameOrigin(closeButtonFrame.origin)
+    
+    minimizeButtonFrame.origin.x = x + buttonSpacingX
+    minimizeButton.setFrameOrigin(minimizeButtonFrame.origin)
+    
+    fullscreenButtonFrame.origin.x = x + 2 * buttonSpacingX
+    fullscreenButton.setFrameOrigin(fullscreenButtonFrame.origin)
+  }
+}
