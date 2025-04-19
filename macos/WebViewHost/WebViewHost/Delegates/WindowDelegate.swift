f6e38971a9cab5a14b9f5b14390f49c5455f381d
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-04-19 15:28:03 +0200

    fix: add missing traffic lights positioning after exiting fullscreen

--- macos/WebViewHost/WebViewHost/Delegates/WindowDelegate.swift
+++ macos/WebViewHost/WebViewHost/Delegates/WindowDelegate.swift
@@ -17,4 +17,14 @@ final class WindowDelegate: NSObject, NSWindowDelegate {
       )
     }
   }
+  
+  func windowDidExitFullScreen(_ notification: Notification) {
+    if let window = NSApp.windows.first {
+      positionTrafficLights(
+        window,
+        x: WindowConstants.TRAFFIC_LIGHTS_INSET_X,
+        y: WindowConstants.TRAFFIC_LIGHTS_INSET_Y
+      )
+    }
+  }
 }
