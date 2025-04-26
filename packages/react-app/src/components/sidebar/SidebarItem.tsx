e699f9b1a2360126b71c54e21618fe0c03e9dce7
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-04-26 22:34:40 +0200

    feat: add navigation to sidebar item

--- packages/react-app/src/components/sidebar/SidebarItem.tsx
+++ packages/react-app/src/components/sidebar/SidebarItem.tsx
@@ -1,10 +1,13 @@
+import { Link } from "../../routing";
 import type { SidebarItemProps } from "./types";
 
 const SidebarItem = ({ label, selected }: SidebarItemProps) => {
   return (
-    <div className={`sidebar-item ${selected ? "selected" : ""}`}>
-      <span className="sidebar-item-label">{label}</span>
-    </div>
+    <Link to="/settings">
+      <div className={`sidebar-item ${selected ? "selected" : ""}`}>
+        <span className="sidebar-item-label">{label}</span>
+      </div>
+    </Link>
   );
 };
 
