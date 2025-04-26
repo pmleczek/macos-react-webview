ce3b8bfe7c71c588037fc44bc8a8f377de868657
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-04-27 00:23:10 +0200

    chore: temporarily unconditionally render component in route

--- packages/router/src/components/Route/Route.tsx
+++ packages/router/src/components/Route/Route.tsx
@@ -1,7 +1,9 @@
 import type { RouteProps } from "./types";
 
-const Route = <T,>({ name }: RouteProps<T>) => {
-  return null;
+const Route = <T,>({ component, name }: RouteProps<T>) => {
+  const RouteComponent = component;
+
+  return <RouteComponent />;
 };
 
 export default Route;
--- packages/router/src/components/Route/types.ts
+++ packages/router/src/components/Route/types.ts
@@ -1,3 +1,7 @@
+import type React from "react";
+
 export interface RouteProps<T> {
+  // TODO: replace with a proper type
+  component: React.ComponentType<any>;
   name: keyof T;
 }
