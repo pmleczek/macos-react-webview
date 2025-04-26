00de86c49ff481fba4e2d41e1609976d7a1f4a07
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-04-26 16:19:27 +0200

    feat: add basic settings open event handling to react-app

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useRef, useState } from "react";
+import { on, off } from "ipc";
+
 import { HeaderBar } from "@components";
+import { EventType } from "@utils/constants";
 
 const App = () => {
+  const [showSettings, setShowSettings] = useState(false);
+  const listenerRef = useRef<string>(null);
+
+  useEffect(() => {
+    listenerRef.current = on(EventType.MENU_ITEM_SETTINGS, () => {
+      setShowSettings(true);
+    });
+
+    return () => {
+      if (listenerRef.current) {
+        off(listenerRef.current);
+        listenerRef.current = null;
+      }
+    };
+  }, []);
+
   return (
     <div id="container">
       <HeaderBar />
       <div id="main-container">
-        <span>Test</span>
+        <span>
+          Test{" "}
+          {showSettings && (
+            <span>Settings (CMD + ,) menu item was selected</span>
+          )}
+        </span>
       </div>
     </div>
   );
--- packages/react-app/src/utils/constants.ts
+++ packages/react-app/src/utils/constants.ts
@@ -1,4 +1,5 @@
 export enum EventType {
+  MENU_ITEM_SETTINGS = "menu-item:settings",
   WINDOW_MAXIMIZE = "window:maximize",
   WINDOW_START_DRAGGING = "window:start-dragging",
   WINDOW_STOP_DRAGGING = "window:stop-dragging",
