e52cbfa4f1fc78118326b4cd6568e8363971e93f
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-04-27 00:23:46 +0200

    chore: add mock implementation of link to router package

--- packages/router/src/components/Link/Link.tsx
+++ packages/router/src/components/Link/Link.tsx
@@ -1,5 +1,15 @@
-const Link = <T,>() => {
-  return null;
+import type { LinkProps } from "./types";
+
+const Link = <T,>({ children, to }: LinkProps<T>) => {
+  const handleClick = () => {
+    console.log(`Link clicked. Navigate to: ${String(to)}`);
+  };
+
+  return (
+    <button type="button" onClick={handleClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Link;
