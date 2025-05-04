3e933ef2648d887446af472863beb64a476bf61d
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-05-05 00:25:12 +0200

    refactor: remove context menu context from react-app

--- packages/react-app/src/contexts/ContextMenu.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ContextMenuContextType } from "./types";
-
-const ContextMenuContext = React.createContext<ContextMenuContextType>(null);
-
-export default ContextMenuContext;
