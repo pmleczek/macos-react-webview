10537b4609364e6d0c1c0a7e2675ea00abfb918e
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-05-18 00:09:58 +0200

    feat: add support for sending events with payload to the ipc package

--- packages/ipc/src/emit.ts
+++ packages/ipc/src/emit.ts
@@ -1,3 +1,7 @@
-export function emit(type: string) {
-  window?.webkit?.messageHandlers?.ipc?.postMessage(type);
+export function emit(type: string, payload?: Object) {
+  const message = JSON.stringify({
+    type,
+    payload,
+  });
+  window?.webkit?.messageHandlers?.ipc?.postMessage(message);
 }
