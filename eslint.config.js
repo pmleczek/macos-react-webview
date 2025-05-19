22047c910335c395b81625f2e1141da2c27f2505
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-05-19 19:23:53 +0200

    chore: add eslint.config.js to monorepo root

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettierPluginRecommendedConfig from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    ignores: ['dist/', 'macos/', 'node_modules/', 'vite-env.d.ts', 'index.d.ts'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  react.configs.flat['jsx-runtime'],
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
+  jsxA11y.flatConfigs.recommended,
+  prettierPluginRecommendedConfig,
+];
