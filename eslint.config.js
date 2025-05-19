fbc622d940978d6dacc295cd87653b0485c4455f
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-05-19 19:29:20 +0200

    chore: add plugin-simple-import-sort to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import prettierPluginRecommendedConfig from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
   {
@@ -17,4 +18,13 @@ export default [
   reactRefresh.configs.vite,
   jsxA11y.flatConfigs.recommended,
   prettierPluginRecommendedConfig,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
 ];
