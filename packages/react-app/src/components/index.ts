16a56dfbb8be041f35d83136a3f07fc4899612a8
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-05-24 22:32:59 +0200

    refactor: remove header bar component

--- packages/react-app/src/components/index.ts
+++ packages/react-app/src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Button } from "./button";
 export { default as ContextMenu } from "./context-menu";
-export { default as HeaderBar } from "./header-bar";
 export { default as Modal } from "./modal";
 export { Table } from "./table";
 export { default as TextInput } from "./text-input";
