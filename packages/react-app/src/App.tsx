1de1be8fa84f4a4f66c7304cc46b76d017bba4c1
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-06-08 12:09:24 +0200

    feat: set up react-query in react-app

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,34 +1,37 @@
-import { createHashRouter, RouterProvider } from "react-router";
-import "ui/dist/index.css";
+import { createHashRouter, RouterProvider } from 'react-router';
+import 'ui/dist/index.css';
 
-import { Menu } from "@components/context-menu";
-import { Home, Settings, Space, Table } from "@pages";
+import { Menu } from '@components/context-menu';
+import { Home, Settings, Space, Table } from '@pages';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createHashRouter([
   {
-    path: "/",
+    path: '/',
     Component: Home,
   },
   {
-    path: "/table",
+    path: '/table',
     Component: Table,
   },
   {
-    path: "/settings",
+    path: '/settings',
     Component: Settings,
   },
   {
-    path: "/spaces/:id",
+    path: '/spaces/:id',
     Component: Space,
-  }
+  },
 ]);
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Menu />
-    </>
+    </QueryClientProvider>
   );
 };
 
