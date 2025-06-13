8e3646401840262149dbedd864833c71f92edb02
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-06-13 21:10:26 +0200

    build: initialize macos app at macos/WebViewHost

---
+++ macos/WebViewHost/WebViewHost/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  WebViewHost
+//
+//  Created by Patryk Mleczek on 6/13/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ macos/WebViewHost/WebViewHost/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WebViewHost
+//
+//  Created by Patryk Mleczek on 6/13/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
