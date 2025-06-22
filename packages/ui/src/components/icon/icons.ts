7b9d4f8d5b557a3e5634478f4098ecd6821ef192
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-06-22 13:19:44 +0200

    feat: adjust components and application for dark mode

--- packages/ui/src/components/icon/icons.ts
+++ packages/ui/src/components/icon/icons.ts
@@ -1,17 +1,22 @@
 import {
+  AppWindowMac,
   Bell,
   ChevronRight,
   Home,
+  Laptop,
+  Moon,
   MoveLeft,
   MoveRight,
   Plus,
   Settings,
   Smile,
+  Sun,
   Table2,
   X,
 } from 'lucide-react';
 
 const icons = {
+  app_window: AppWindowMac,
   arrow_left: MoveLeft,
   arrow_right: MoveRight,
   bell: Bell,
@@ -19,8 +24,11 @@ const icons = {
   close: X,
   emoji: Smile,
   home: Home,
+  laptop: Laptop,
+  moon: Moon,
   plus: Plus,
   settings: Settings,
+  sun: Sun,
   table: Table2,
 } as const;
 
